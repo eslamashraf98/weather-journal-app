2b5639ca356c5b635b664911fb026f1f14eae2d6
ESLAM <102992749+eslamashraf98@users.noreply.github.com>
2022-10-10 21:43:11 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,10 +1,11 @@
-//* Global Variables */
+/* Global Variables */
 const baseLink = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const key = '&appid=fe7ad27a476129f711459d73facb6b84&units=metric';
+const generateBtn = document.querySelector('#generate');
 
 // Create a new date instance dynamically with JS
 let date = new Date ();
-let newDate = date.getDay() + '.' + date.getMonth() + '.' + date.getFullYear();
+let newDate = date.getMonth() + 1 + '.' + date.getDate() + '.' + date.getFullYear();
 function start(x) {
     const zipCode = document.querySelector('#zip').value;
     const feel = document.querySelector('#feelings').value;
@@ -13,14 +14,14 @@ function start(x) {
     Temperature(baseLink, zipCode, key)
         .then(function (info) {
         // Add data to POST request
-            postInfo('/weatherData', { temperature: info.main.temp, date: newDate, user_response: feel })
+            postInfo('/postData', { temperature: info.main.temp, date: newDate, user_response: feel })
             update();
         })
 };
-
+generateBtn.addEventListener('click', start);
 // Async GET
 const Temperature = async (baseLink, zipCode, key) => {
-    const response = await fetch(baseLink + zipCode + key)
+    const response = await fetch(`${baseLink}${zipCode}${key}`)
     console.log(response);
     try {
         const info = await response.json();
@@ -51,7 +52,7 @@ const postInfo = async (link = '', info = {}) => {
     }
 };
 
-// Update user interface
+// Update UI
 const update = async () => {
     const request = await fetch('http://localhost:5000/addData');
     try {
@@ -63,6 +64,6 @@ const update = async () => {
     }
     catch (error) {
         console.log('Error', error);
+// appropriately handle the error
     }
 };
-document.querySelector('#generate').addEventListener('click', start);
